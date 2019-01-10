e09ded552217d3e4500b3dd2c148e704b6063301
Nayyar <pnayyar@rjtcompuquest.com>
2019-01-10 16:34:36 -0600

    Adding API Handler classes.

---
+++ B16_Shopping_App/Utilities/APIHandler.h
@@ -0,0 +1,17 @@
+//
+//  APIHandler.h
+//  B16_Shopping_App
+//
+//  Created by Nayyar on 10/01/19.
+//  Copyright © 2019 Da Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface APIHandler : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
