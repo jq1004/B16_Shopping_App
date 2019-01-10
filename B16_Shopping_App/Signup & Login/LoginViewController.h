0f0a67936a54367303e3434497ffe1a6fe10851a
dachen <2672921697@qq.com>
2019-01-10 15:59:08 -0600

    Add Login Files

--- B16_Shopping_App/Signup & Login/LoginViewController.h
+++ B16_Shopping_App/Signup & Login/LoginViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LoginViewController.h
 //  B16_Shopping_App
 //
 //  Created by Da Chen on 1/10/19.
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
 
+@interface LoginViewController : UIViewController
 
 @end
 
+NS_ASSUME_NONNULL_END
---
+++ B16_Shopping_App/Signup & Login/LoginViewController.m
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.m
+//  B16_Shopping_App
+//
+//  Created by Da Chen on 1/10/19.
+//  Copyright © 2019 Da Chen. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
