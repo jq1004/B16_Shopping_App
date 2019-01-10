08b938dc2b66d3799a785037301ebdeb67b9352c
Mouaddine Hajar <mouaddine.hajar@gmail.com>
2019-01-10 16:35:17 -0600

    Adding APIHandler and APIParser

---
+++ B16_Shopping_App/Services/APIHandler.h
@@ -0,0 +1,15 @@
+//
+//  APIHandler.h
+//  B16_Shopping_App
+//
+//  Created by Hajar Mouaddine on 1/10/19.
+//  Copyright © 2019 Da Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APIHandler
+
++(instancetype)sharedInstance;
+
+@end
---
+++ B16_Shopping_App/Services/APIParser.m
@@ -0,0 +1,9 @@
+//
+//  APIParser.m
+//  B16_Shopping_App
+//
+//  Created by Hajar Mouaddine on 1/10/19.
+//  Copyright © 2019 Da Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
