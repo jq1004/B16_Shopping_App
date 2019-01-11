b6258e868daa2d980e923f395d5e3653d3d5d4c0
dachen <2672921697@qq.com>
2019-01-11 10:19:32 -0600

    Using constants in Login.

--- B16_Shopping_App/Signup & Login/LoginViewController.m
+++ B16_Shopping_App/Signup & Login/LoginViewController.m
@@ -26,13 +26,15 @@
     // Make api call
     NSString *phone = _phoneText.text;
     NSString *pwd = _password.text;
-    NSString *strURL = [NSString stringWithFormat:@"http://rjtmobile.com/aamir/e-commerce/ios-app/shop_login.php?mobile=%@&password=%@", phone, pwd];
-//    NSURL *url = [NSURL URLWithString:strURL];
-    NSURL *url = [NSURL URLWithString:@"http://rjtmobile.com/aamir/e-commerce/ios-app/shop_login.php?mobile=55565454&password=7011"];
+    // Constant header
+    NSString *strURL = [NSString stringWithFormat:@"%@mobile=%@&password=%@", kLOGINAPI, phone, pwd];
+    NSURL *url = [NSURL URLWithString:strURL];
+    
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     [request setHTTPMethod:@"GET"];
     
     NSURLSessionDataTask *dataTask = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
         NSLog(@"%@", data);
     }];
     [dataTask resume];
