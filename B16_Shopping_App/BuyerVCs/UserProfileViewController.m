af35a37edcdb6961109ae267d0b0ef9a4716f1a7
Mouaddine Hajar <mouaddine.hajar@gmail.com>
2019-01-12 00:10:17 -0600

    Added signup and update profiles screens and linked them to their respective view controllers.

--- B16_Shopping_App/BuyerVCs/UserProfileViewController.m
+++ B16_Shopping_App/BuyerVCs/UserProfileViewController.m
@@ -8,7 +8,17 @@
 
 #import "UserProfileViewController.h"
 #import "SWRevealViewController.h"
+#import "APIHandler.h"
+
 @interface UserProfileViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *userProfileImgView;
+@property (weak, nonatomic) IBOutlet UITextField *fnameTxtF;
+@property (weak, nonatomic) IBOutlet UITextField *lnameTxtF;
+@property (weak, nonatomic) IBOutlet UITextField *addressTxtF;
+@property (weak, nonatomic) IBOutlet UITextField *mobileTxtF;
+@property (weak, nonatomic) IBOutlet UITextField *emailTxtF;
+
 
 @end
 
@@ -16,21 +26,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     _menuBarBtn.target = self.revealViewController;
     _menuBarBtn.action = @selector(revealToggle:);
+    
+    _userProfileImgView.layer.borderWidth = 2;
+    _userProfileImgView.layer.borderColor = [UIColor colorWithRed:1.00 green:0.23 blue:0.82 alpha:1.0].CGColor;
+    _userProfileImgView.layer.cornerRadius = _userProfileImgView.frame.size.width / 2;
     
     [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
     
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)updateProfileBtnClicked:(UIButton *)sender {
+    if (![self.fnameTxtF.text  isEqual: @""] && ![self.lnameTxtF.text  isEqual: @""] && ![self.mobileTxtF.text  isEqual: @""] && ![self.addressTxtF.text  isEqual: @""] && ![self.emailTxtF.text  isEqual: @""])
+    {
+        
+        [[APIHandler sharedInstance] updateProfileUserWithFirstName:self.fnameTxtF.text lastName:self.lnameTxtF.text address:self.addressTxtF.text email:self.emailTxtF.text mobile:self.mobileTxtF.text completionHandler:^(NSString *response) {
+            if ([response isEqual: @"successfully updated"]) {
+                //show success message
+            } else {
+                //show error message
+            }
+        }];
+        
+    } else {
+        //show error : all required fields should be filled
+    }
 }
-*/
 
 @end
--- B16_Shopping_App/Signup & Login/SignUpViewController.m
+++ B16_Shopping_App/Signup & Login/SignUpViewController.m
@@ -12,6 +12,20 @@
 
 @interface SignUpViewController()
 
+@property (weak, nonatomic) IBOutlet UITextField *fnameTxtF;
+@property (weak, nonatomic) IBOutlet UITextField *lnameTxtF;
+@property (weak, nonatomic) IBOutlet UITextField *mobileTxtF;
+@property (weak, nonatomic) IBOutlet UITextField *addressTxtF;
+@property (weak, nonatomic) IBOutlet UITextField *emailTxtF;
+@property (weak, nonatomic) IBOutlet UITextField *pwdTxtF;
+@property (weak, nonatomic) IBOutlet UITextField *cpwdTxtF;
+@property (weak, nonatomic) IBOutlet UIButton *buyerBtn;
+@property (weak, nonatomic) IBOutlet UIButton *sellerBtn;
+@property (weak, nonatomic) IBOutlet UITextField *taxIdTxtF;
+@property (weak, nonatomic) IBOutlet UIButton *backBtn;
+
+@property NSString *UserType;
+
 @end
 
 @implementation SignUpViewController
@@ -19,6 +33,54 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.backBtn.layer.borderWidth = 1;
+    self.backBtn.layer.borderColor = [UIColor colorWithRed:1.00 green:0.23 blue:0.82 alpha:1.0].CGColor;
+    self.taxIdTxtF.hidden = true;
+    self.sellerBtn.imageView.image = [UIImage imageNamed:@"radioE"];
+    self.UserType = @"Buyer";
+}
+
+- (IBAction)buyerBtnClicked:(UIButton *)sender {
+    self.UserType = @"Buyer";
+    self.buyerBtn.imageView.image = [UIImage imageNamed:@"radioF"];
+    self.sellerBtn.imageView.image = [UIImage imageNamed:@"radioE"];
+    self.taxIdTxtF.hidden = true;
+}
+
+- (IBAction)sellerBtnClicked:(UIButton *)sender {
+    self.UserType = @"Seller";
+    self.buyerBtn.imageView.image = [UIImage imageNamed:@"radioE"];
+    self.sellerBtn.imageView.image = [UIImage imageNamed:@"radioF"];
+    self.taxIdTxtF.hidden = false;
+}
+
+- (IBAction)signUpBtnClicked:(UIButton *)sender {
+    
+    if (![self.fnameTxtF.text  isEqual: @""] && ![self.lnameTxtF.text  isEqual: @""] && ![self.mobileTxtF.text  isEqual: @""] && ![self.addressTxtF.text  isEqual: @""] && ![self.emailTxtF.text  isEqual: @""] && ![self.pwdTxtF.text  isEqual: @""] && ![self.cpwdTxtF.text  isEqual: @""] && ![self.UserType  isEqual: @""]) {
+        
+        if ([self.pwdTxtF.text isEqual: self.cpwdTxtF.text]) {
+            
+            [[APIHandler sharedInstance] signUpUserWithFirstName:self.fnameTxtF.text lastName:self.lnameTxtF.text address:self.addressTxtF.text password:self.pwdTxtF.text email:self.emailTxtF.text mobile:self.mobileTxtF.text completionHandler:^(NSString *response) {
+                if ([response isEqual:@"successfully registered"]) {
+                    [self dismissViewControllerAnimated:true completion:nil];
+                    //show success message
+                } else {
+                    //show error message
+                }
+            }];
+            
+        } else {
+            //show error : password mismatch
+        }
+        
+    } else {
+        //show error : fill in all reauired fields
+    }
+}
+
+- (IBAction)backBtnClicked:(UIButton *)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
 }
 
 @end
