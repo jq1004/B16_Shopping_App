386abd9a1c04b5874dd5cf59e58edf45fa25ed3f
Mouaddine Hajar <mouaddine.hajar@gmail.com>
2019-01-16 18:31:39 -0600

    Solved merge conflicts

--- B16_Shopping_App/Utilities/B16_Shopping_App_Constants.h
+++ B16_Shopping_App/Utilities/B16_Shopping_App_Constants.h
@@ -9,19 +9,6 @@
 #ifndef B16_Shopping_App_Constants_h
 #define B16_Shopping_App_Constants_h
 
-#define kLOGINAPI @"http://rjtmobile.com/aamir/e-commerce/ios-app/shop_login.php?"
-#define kFORGETPWDAPI @"http://rjtmobile.com/aamir/e-commerce/ios-app/forgot_pass_email.php?"
-#define KSIGNUPAPI @"http://rjtmobile.com/aamir/e-commerce/ios-app/shop_reg.php?"
-#define KUPDATEPROFILEAPI @"http://rjtmobile.com/aamir/e-commerce/ios-app/edit_profile.php?"
-#define KRESETPASSWORDAPI @"http://rjtmobile.com/aamir/e-commerce/ios-app/shop_reset_pass.php?"
-#define kCATEGORYAPI @"http://rjtmobile.com/ansari/shopingcart/ios-ssl/cust_category.php?"
-#define kTOPSELLERSAPI @"http://rjtmobile.com/aamir/e-commerce/ios-app/shop_top_sellers.php?"
-#define kPRODUCTAPI @"http://rjtmobile.com/ansari/shopingcart/ios-ssl/product_details.php?"
-#define kSUBCATEGORYAPI @"http://rjtmobile.com/ansari/shopingcart/ios-ssl/cust_sub_category.php?"
-#define kCOUPONAPI @"http://rjtmobile.com/aamir/e-commerce/ios-app/coupon.php?"
-#define kORDERAPI @"http://rjtmobile.com/aamir/e-commerce/ios-app/orders.php?"
-#define kORDERHISTORY @"http://rjtmobile.com/aamir/e-commerce/ios-app/order_history.php?"
-#define kSHIPMENTTRACKAPI @"http://rjtmobile.com/aamir/e-commerce/ios-app/shipment_track.php?"
 // API constants
 #define kLOGINAPI               @"http://rjtmobile.com/aamir/e-commerce/ios-app/shop_login.php?"
 #define kFORGETPWDAPI           @"http://rjtmobile.com/aamir/e-commerce/ios-app/forgot_pass_email.php?"
@@ -34,6 +21,8 @@
 #define kSUBCATEGORYAPI         @"http://rjtmobile.com/ansari/shopingcart/ios-ssl/cust_sub_category.php?"
 #define kCOUPONAPI              @"http://rjtmobile.com/aamir/e-commerce/ios-app/coupon.php?"
 #define kORDERAPI               @"http://rjtmobile.com/aamir/e-commerce/ios-app/orders.php?"
+#define kORDERHISTORY @"http://rjtmobile.com/aamir/e-commerce/ios-app/order_history.php?"
+#define kSHIPMENTTRACKAPI @"http://rjtmobile.com/aamir/e-commerce/ios-app/shipment_track.php?"
 
 // Login constants
 #define kNoDataError            @"There is no data in this object."
